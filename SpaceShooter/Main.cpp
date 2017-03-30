1d61eecb482448fded3619808948069f37decf13
Vilunen <vilunen@gmail.com>
2017-03-31 01:02:54 +0300

    Bullet counter

--- SpaceShooter/Main.cpp
+++ SpaceShooter/Main.cpp
@@ -99,7 +99,7 @@ void Run() {
 		frames++;
 
 		if (time >= std::chrono::nanoseconds(1000000000)) {
-			printf("Frames: %d, Ticks: %d\n", frames, ticks);
+			printf("Frames: %d, Ticks: %d, Bullets: %d\n", frames, ticks, game->GetPlayer()->GetBullets().size());
 			ticks = 0;
 			frames = 0;
 			time = std::chrono::nanoseconds(0);
